6d2f08285a9c0e686d05af21ddd417940fee5565
RoguePetz <briggsrhema@gmail.com>
2024-10-06 20:37:20 +0100

    cors feature

--- server.js
+++ server.js
@@ -16,7 +16,11 @@ const postRoute = require('./routes/postRoutes');
 connectDB();
 
 //middleware
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000', // Allow requests from this origin
+    methods: 'GET,POST,PUT,DELETE', // Specify allowed HTTP methods
+    credentials: true // Allow credentials if needed
+}));
 app.use(userRoutes);
 app.use(bodyParser.json());
 
